d47d87579a603931d8ac84d64be54b92f20dc6d2
Michael Ebert <ebert@ad-hoc.network>
2017-03-26 09:58:20 +0200

    updated Mindmap with literature ideas

--- Mindmap/FreeRtos - Seminar Betriebsysteme .mm
+++ Mindmap/FreeRtos - Seminar Betriebsysteme .mm
@@ -1,4 +1,20 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1490378120399" ID="ID_699230232" MODIFIED="1490378145258" TEXT="FreeRtos - Seminar Betriebsysteme "/>
+<node CREATED="1490378120399" ID="ID_699230232" MODIFIED="1490514867205" TEXT="FreeRtos - Seminar Betriebsysteme ">
+<node CREATED="1490513756664" ID="ID_1225602725" MODIFIED="1490513833213" POSITION="right" TEXT="Literatur">
+<node CREATED="1490513809557" ID="ID_1780518892" MODIFIED="1490513838638" TEXT="http://www.freertos.org/"/>
+<node CREATED="1490513869428" ID="ID_263935073" MODIFIED="1490513943850" TEXT="&#x9;Mastering the FreeRTOS Real Time Kernel - a Hands On Tutorial Guide (Richard Barry)"/>
+<node CREATED="1490513947551" ID="ID_951067781" MODIFIED="1490513962020" TEXT="&#x9;FreeRTOS V9.0.0 Reference Manual"/>
+<node CREATED="1490514078265" ID="ID_1094485887" MODIFIED="1490514080330" TEXT="Definitive Guide to ARM Cortex-M3 and Cortex-M4 Processors"/>
+<node CREATED="1490514447659" ID="ID_1795139287" MODIFIED="1490514456759" TEXT="Real-Time Embedded Systems&#x2014;Open-Source Operating Systems Perspective">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1490514482109" ID="ID_1232787287" MODIFIED="1490514814788" TEXT="Building a Real Time Operating System: Rtos from the Ground Up">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1490514822929" ID="ID_1468315989" MODIFIED="1490514861781" TEXT="Modern Operating Systems - Tanenbaum"/>
+</node>
+<node CREATED="1490514871954" ID="ID_1656633342" MODIFIED="1490514888636" POSITION="left" TEXT="Kapitel Vorschl&#xe4;ge"/>
+<node CREATED="1490514894038" ID="ID_1659781155" MODIFIED="1490514902621" POSITION="right" TEXT="Vortragsideen"/>
+</node>
 </map>
